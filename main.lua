8d5a6a6d1315715f3ea6c25d9f359745f118cc69
salat20 <150575974+Bbblaxtop@users.noreply.github.com>
2025-01-24 21:13:57 +0500

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,28 @@
+local function ScannerAPI()
+	-- Scanner API
+	-- Backdoor Scanner for scripts and etc.
+
+	-- Bypass FE and execute scripts on Server-Side
+
+	-- by salat20
+	
+	warn("Scanner API | Scanning...")
+
+	for i,v in pairs(game:GetDescendants()) do
+		if v:IsA("RemoteEvent") then
+			warn("Scanner API | Hooking in "..v.Name.."...")
+
+			v:FireServer("local a = Instance.new'IntValue'; a.Parent = workspace; a.Name = 'ScannerAPI';")
+
+			task.wait(2)
+
+			if workspace.ScannerAPI then
+				warn("Scanner API | Hooked "..v.Name.."!")
+				_G.ScannerAPIEvent = v
+				break
+			end
+		end
+	end
+	
+	return _G.ScannerAPIEvent
+end
